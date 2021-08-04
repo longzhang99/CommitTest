cf5bb576b67c8e4d192e710cac62e6b43abea91d
zhanglong <zhanglong10@asiainfo.com>
2021-08-04 21:37:34 +0800

    My first commit in eclipse

---
+++ src/helloworld/CommitTest.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class CommitTest {
+	public static void main(String[] args) {
+		System.out.println("My first commit in eclipse to git");
+		System.out.println("It is so cool!" + " " + "right?");
+	}
+}
